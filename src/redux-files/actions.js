27ad9e95f5fa3e0e354aef4d45b7dc0291471bf2
Christian Pinto <pintochristian2@gmail.com>
2019-09-07 18:06:06 -0400

    updated fetch link

--- src/redux-files/actions.js
+++ src/redux-files/actions.js
@@ -2,7 +2,7 @@ import {FETCH, PRODUCT, CHOSENACCESSORIES, ADDON } from './types'
 
 function fetchProducts(setProducts){
     return function(dispatch){
-      fetch("http://localhost:3000/products")
+      fetch("https://topview-api.herokuapp.com/products")
       .then(resp => resp.json())
       .then(products => {
         dispatch({type: FETCH, payload: products.products})
